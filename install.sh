ac3a6dc8cc2a789a07a1048c498004e38030c15d
anorberg001-web <anorberg001@stpaul.k12.mn.us>
2026-09-17 18:20:56 -0500

    Change installation directory to .astral-build

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-
 #!/usr/bin/env bash
 set -euo pipefail
 
-mkdir -p bin
-install -m 0755 neofetch bin/neofetch
+mkdir -p .astral-build
+cp neofetch .astral-build/neofetch
+chmod 0755 .astral-build/neofetch
